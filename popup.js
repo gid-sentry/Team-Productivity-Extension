b817495995cf6a804923ddb6ebc85b265a03e1a8
Gideon Lapshun <gideon.lapshun@sentry.io>
2022-12-15 16:20:52 -0500

    update to table format

--- popup.js
+++ popup.js
@@ -28,25 +28,48 @@ async function getTeamIssues(org,team){
   return numIssues
 }
 
-
-function addTeamSpan(sortedArray,org) {
+function addTeamSpan(sortedArray) {
   sortedArray.reverse();
+  let tbl = document.createElement('table');
+  tbl.setAttribute('id','table')
+  tbl.style.width = '100px';
+  tbl.style.border = '1px solid black';
+  const firstRow = tbl.insertRow()
+  const firstCell = firstRow.insertCell()
+  const secondCell = firstRow.insertCell()
+  var text = document.createTextNode("Team name")
+  firstCell.appendChild(text);
+  firstCell.style.border = '1px solid black';
+  text = document.createTextNode("Unresovled issues")
+  secondCell.appendChild(text);
+  secondCell.style.border = '1px solid black';
   sortedArray.forEach(key => {
-    let newDiv = document.createElement('div');
-    newDiv.classList.add('teams');
-    newDiv.setAttribute("id", "team")
-    document.body.appendChild(newDiv)
-    let newSpan = document.createElement('span')
-    let text = document.createTextNode(key + ":" +teamDict[key])
-    newSpan.appendChild(text)
-    newSpan.title = key + ":" +teamDict[key];
-    newSpan.href = "https://sentry.io/organizations/"+org+"issues/is%3Aunresolved+assigned%3A%23"+key+"&referrer=issue-list&statsPeriod=90d"
-    // newSpan.innerText = key + ":" +teamDict[key]
-    // newSpan.href = "https://sentry.io/organizations/"+org+"issues/is%3Aunresolved+assigned%3A%23"+key+"&referrer=issue-list&statsPeriod=14d"
-    newDiv.append(newSpan)
-    newSpan.href = "https://sentry.io/organizations/"+org+"issues/is%3Aunresolved+assigned%3A%23"+key+"&referrer=issue-list&statsPeriod=90d"
-    newSpan.style.fontSize = "large";
+    const tr = tbl.insertRow()
+    const teamName = tr.insertCell()
+    const teamIssues = tr.insertCell()
+    text = document.createTextNode(key)
+    teamName.appendChild(text)
+    teamName.style.border = '1px solid black';
+    text = document.createTextNode(teamDict[key])
+    teamIssues.appendChild(text)
+    teamIssues.style.border = '1px solid black';
+    // Adding text divs
+    // let newDiv = document.createElement('div');
+    // newDiv.classList.add('teams');
+    // newDiv.setAttribute("id", "team")
+    // document.body.appendChild(newDiv)
+    // let newSpan = document.createElement('span')
+    // let text = document.createTextNode(key + ":" +teamDict[key])
+    // newSpan.appendChild(text)
+    // newSpan.title = key + ":" +teamDict[key];
+    // newSpan.href = "https://sentry.io/organizations/"+org+"issues/is%3Aunresolved+assigned%3A%23"+key+"&referrer=issue-list&statsPeriod=90d"
+    // // newSpan.innerText = key + ":" +teamDict[key]
+    // // newSpan.href = "https://sentry.io/organizations/"+org+"issues/is%3Aunresolved+assigned%3A%23"+key+"&referrer=issue-list&statsPeriod=14d"
+    // newDiv.append(newSpan)
+    // newSpan.href = "https://sentry.io/organizations/"+org+"issues/is%3Aunresolved+assigned%3A%23"+key+"&referrer=issue-list&statsPeriod=90d"
+    // newSpan.style.fontSize = "large";
 })
+document.body.appendChild(tbl);
 }
 
 function sortTeams(teamDictionary){
@@ -63,12 +86,14 @@ function sortTeams(teamDictionary){
 
   return keys
 }
+
 const teamsQuery = "teams/"
 const issueQuery = "issues/?collapse=stats&expand=owners&expand=inbox&limit=1000&query=is:unresolved assigned:%23"
 const issueAppend = "&shortIdLookup=1&statsPeriod=90d"
 let teamDict = {}
 var selectedNumberOfIssues = document.getElementById("teamNumbers");
-selectedNumberOfIssues.addEventListener("change",onChangeSelection(this))
+selectedNumberOfIssues.addEventListener("change",()=>{ document.getElementById('table').remove(); addTeamSpan(sortedTeams.slice(-selectedNumberOfIssues.value));
+})
 var checkOpen = 0
 var sortedTeams = []
 currentOrg();
@@ -77,7 +102,7 @@ function start(org){
     console.log(org)
     const teams = await getTeams(org)
     selectedNumberOfIssues = document.getElementById("teamNumbers");
-    selectedNumberOfIssues.addEventListener("change",onChangeSelection(this))
+    // selectedNumberOfIssues.addEventListener("change",onChangeSelection(this))
     teams.forEach(element => {
   
         var timeNow = Date.now()
@@ -95,7 +120,7 @@ function start(org){
   
           if (sortedTeams.length == teams.length-1) {
             var sliceNumber = selectedNumberOfIssues.value * -1
-            addTeamSpan(sortedTeams.slice(sliceNumber),org)
+            addTeamSpan(sortedTeams.slice(sliceNumber))
             checkOpen+=1
           }
         })
@@ -105,16 +130,3 @@ function start(org){
 
 }
 
-
-function onChangeSelection(option) {
-  // console.log('option is ')
-  // console.log(option)
-  // [...document.getElementsByClassName("teams")].map(n => n && n.remove());
-  //  selectedNumberOfIssues = document.getElementById("teamNumbers").value;
-  // var sliceNumber = document.getElementById("teamNumbers").value * -1
-  // console.log("slice number is ")
-  // console.log(sliceNumber)
-  // if (checkOpen > 0){
-  //   addTeamSpan(sortedTeams.slice(sliceNumber))
-  // }
-}
