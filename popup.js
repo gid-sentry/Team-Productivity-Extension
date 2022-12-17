a323613895b2f6e496c4c596ae9c23cfd0198655
Gideon Lapshun <gideon.lapshun@sentry.io>
2022-12-17 17:50:14 -0500

    a bit of code cleanup

--- popup.js
+++ popup.js
@@ -1,74 +1,55 @@
-const api = "https://sentry.io/api/0/organizations/"
+const api = "https://sentry.io/api/0/organizations/";
 let url;
 
 function currentOrg(){
-  // var org = 'testorg-az/'
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
-    url = activeTab.url; 
-    const org = url.split('/')[4] + "/"
+    url = activeTab.url; ;
+    const org = url.split('/')[4] + "/";
     start(org);
   });
-  // return org;
-  // return "sentry/";
-  // return "testorg-az/";
-  // return "zendesk-us/";
 
 }
 
 async function getTeams(org){
-  const teams = await fetch(api+org+teamsQuery).then(r => r.json())
-  return teams
+  const teams = await fetch(api+org+teamsQuery).then(r => r.json());
+  return teams;
 }
 
 async function getTeamIssues(org,team){
   const numIssues = await fetch(api+org+issueQuery+team['name']+issueAppend).then(r=>r.json()).then(result => {
-    return result.length
-  })
-  return numIssues
+    return result.length;
+  });
+  return numIssues;
 }
 
 function addTeamSpan(sortedArray) {
   sortedArray.reverse();
   let tbl = document.createElement('table');
-  tbl.setAttribute('id','table')
+  tbl.setAttribute('id','table');
   tbl.style.width = '100px';
   tbl.style.border = '1px solid black';
-  const firstRow = tbl.insertRow()
-  const firstCell = firstRow.insertCell()
-  const secondCell = firstRow.insertCell()
-  var text = document.createTextNode("Team name")
+  const firstRow = tbl.insertRow();
+  const firstCell = firstRow.insertCell();
+  const secondCell = firstRow.insertCell();
+  var text = document.createTextNode("Team name");
   firstCell.appendChild(text);
   firstCell.style.border = '1px solid black';
-  text = document.createTextNode("Unresovled issues")
+  text = document.createTextNode("Unresovled issues");
   secondCell.appendChild(text);
   secondCell.style.border = '1px solid black';
   sortedArray.forEach(key => {
-    const tr = tbl.insertRow()
-    const teamName = tr.insertCell()
-    const teamIssues = tr.insertCell()
-    text = document.createTextNode(key)
-    teamName.appendChild(text)
+    const tr = tbl.insertRow();
+    const teamName = tr.insertCell();
+    const teamIssues = tr.insertCell();
+    text = document.createTextNode(key);
+    teamName.appendChild(text);
     teamName.style.border = '1px solid black';
-    text = document.createTextNode(teamDict[key])
-    teamIssues.appendChild(text)
+    text = document.createTextNode(teamDict[key]);
+    teamIssues.appendChild(text);
     teamIssues.style.border = '1px solid black';
-    // Adding text divs
-    // let newDiv = document.createElement('div');
-    // newDiv.classList.add('teams');
-    // newDiv.setAttribute("id", "team")
-    // document.body.appendChild(newDiv)
-    // let newSpan = document.createElement('span')
-    // let text = document.createTextNode(key + ":" +teamDict[key])
-    // newSpan.appendChild(text)
-    // newSpan.title = key + ":" +teamDict[key];
-    // newSpan.href = "https://sentry.io/organizations/"+org+"issues/is%3Aunresolved+assigned%3A%23"+key+"&referrer=issue-list&statsPeriod=90d"
-    // // newSpan.innerText = key + ":" +teamDict[key]
-    // // newSpan.href = "https://sentry.io/organizations/"+org+"issues/is%3Aunresolved+assigned%3A%23"+key+"&referrer=issue-list&statsPeriod=14d"
-    // newDiv.append(newSpan)
-    // newSpan.href = "https://sentry.io/organizations/"+org+"issues/is%3Aunresolved+assigned%3A%23"+key+"&referrer=issue-list&statsPeriod=90d"
-    // newSpan.style.fontSize = "large";
-})
+
+});
 document.body.appendChild(tbl);
 }
 
@@ -84,49 +65,47 @@ function sortTeams(teamDictionary){
   var keys = items.map(
     (e) => { return e[0] });
 
-  return keys
+  return keys;
 }
 
-const teamsQuery = "teams/"
-const issueQuery = "issues/?collapse=stats&expand=owners&expand=inbox&limit=1000&query=is:unresolved assigned:%23"
-const issueAppend = "&shortIdLookup=1&statsPeriod=90d"
+const teamsQuery = "teams/";
+const issueQuery = "issues/?collapse=stats&expand=owners&expand=inbox&limit=1000&query=is:unresolved assigned:%23";
+const issueAppend = "&shortIdLookup=1&statsPeriod=90d";
 let teamDict = {}
 var selectedNumberOfIssues = document.getElementById("teamNumbers");
 selectedNumberOfIssues.addEventListener("change",()=>{ document.getElementById('table').remove(); addTeamSpan(sortedTeams.slice(-selectedNumberOfIssues.value));
-})
-var checkOpen = 0
-var sortedTeams = []
+});
+var checkOpen = 0;
+var sortedTeams = [];
 currentOrg();
 function start(org){
   ;(async () => { 
-    console.log(org)
-    const teams = await getTeams(org)
+    const teams = await getTeams(org);
     selectedNumberOfIssues = document.getElementById("teamNumbers");
-    // selectedNumberOfIssues.addEventListener("change",onChangeSelection(this))
     teams.forEach(element => {
   
-        var timeNow = Date.now()
+        var timeNow = Date.now();
         while(Date.now()<timeNow+120){
-          var x = 1
+          var x = 1;
         }
-        ;(async () => { 
-          const issueNumber = await getTeamIssues(org,element)
-          teamDict[element['name']]=issueNumber
-          sortedTeams = sortTeams(teamDict)
+        ;(async () => { ;
+          const issueNumber = await getTeamIssues(org,element);
+          teamDict[element['name']]=issueNumber;
+          sortedTeams = sortTeams(teamDict);
           if (sortedTeams.length == teams.length) {
             return sortedTeams;
           }
         })().then(() => {
   
           if (sortedTeams.length == teams.length-1) {
-            var sliceNumber = selectedNumberOfIssues.value * -1
-            addTeamSpan(sortedTeams.slice(sliceNumber))
-            checkOpen+=1
+            var sliceNumber = selectedNumberOfIssues.value * -1;
+            addTeamSpan(sortedTeams.slice(sliceNumber));
+            checkOpen+=1;
           }
         })
       })
   
-  })()
+  })();
 
 }
 
